fce210e147c4fe57af4d506a71c6c54e5533cfce
kbmstar2 <33720297+kbmstar2@users.noreply.github.com>
2023-05-15 05:27:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
         score = 0
         while not done:
             action, prob, val = agent.choose_action(observation)
-            observation_, reward, done, info, _ = env.step(action)
+            observation_, reward, done, _ , _ = env.step(action)
             n_steps += 1
             score += reward
             agent.remember(observation, action, prob, val, reward, done)
